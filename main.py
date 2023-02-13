363247141ecf483f7b5071a5d3a27c5f6ca1f21f
Daniil_Manuilov <manuilovdaniil@yandex.ru>
2023-02-13 16:58:40 +0100

    feat: predict salary

--- main.py
+++ main.py
@@ -4,12 +4,14 @@ from sys import stderr
 import requests
 
 
-HEADHUNTER_URL = 'https://api.hh.ru/'
+HEADHUNTER_URL = 'https://api.hh.ru'
 MOSCOW_ID = 1
 MONTH_PERIOD = 30
 
 
-def count_jobs(language):
+def get_jobs(language):
+    """ Функция возвращает вакансии с сайта hh.ru для программирования на определенном языке
+    """
     url = urljoin(HEADHUNTER_URL, '/vacancies')
     payload = {'area': MOSCOW_ID,
                'period': MONTH_PERIOD,
@@ -18,20 +20,38 @@ def count_jobs(language):
     response.raise_for_status()
 
     vacancies = response.json()
-    return vacancies['found']
+    return vacancies
+
+
+def predict_rub_salary(vacancy):
+    """ Функция возвращает ожидаемую зарплату в рублях
+    """
+    salary = vacancy.get('salary')
+    if salary and salary.get('currency') == 'RUR':
+        salary_from = salary.get('from')
+        salary_to = salary.get('to')
+
+        if salary_from and salary_to:
+            return int((salary_from + salary_to) / 2)
+        elif salary_from:
+            return int(1.2 * salary_from)
+        elif salary_to:
+            return int(0.8 * salary_to)
+
+    return None
 
 
 def main():
-    languages = ['java', 'Go', 'C', 'C#', 'C++', 'PHP', 'Ruby', 'java', 'javascript', 'python']
-    programmer_jobs = {}
-    for language in languages:
-        try:
-            programmer_jobs[language] = count_jobs(language)
-        except requests.exceptions.HTTPError:
-            stderr.write(f'Не удалось сделать запрос для языка {language}\n')
+    language = 'python'
+    try:
+        jobs = get_jobs(language)
+        vacancies = jobs.get('items', [])
 
-    for language, jobs_count in sorted(programmer_jobs.items(), key=lambda item: item[1], reverse=True):
-        print(f'{language} - {jobs_count}')
+        for vacancy in vacancies:
+            print(predict_rub_salary(vacancy))
+
+    except requests.exceptions.HTTPError:
+        stderr.write(f'Не удалось сделать запрос для языка {language}\n')
 
 
 if __name__ == '__main__':
